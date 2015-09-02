597493114e64bbfd7ab0fbab024d652877e4c8e4
robinhodemorais <robinhodemorais@gmail.com>
2015-09-01 22:34:02 -0300

    Adicionado Models

---
+++ database/seeds/ProjectTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //\CodeProject\Entities\Project::truncate();
+        factory(\CodeProject\Entities\Project::class,10)->create();
+    }
+}
