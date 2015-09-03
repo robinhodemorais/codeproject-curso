4c4a502d392fc183e65c94fbd534dc8c5a791132
robinhodemorais <robinhodemorais@gmail.com>
2015-09-02 23:18:29 -0300

    Atualizando Projects e Models

---
+++ app/Services/ProjectService.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robinho de Morais
+ * Date: 01/09/2015
+ * Time: 21:11
+ */
+
+namespace CodeProject\Services;
+
+
+use CodeProject\Repositories\ClientRepository;
+use CodeProject\Repositories\ProjectRepository;
+use CodeProject\Validators\ClientValidator;
+use CodeProject\Validators\ProjectValidator;
+use Prettus\Validator\Exceptions\ValidatorException;
+
+
+class ProjectService {
+    protected $repository;
+    /**
+     * @var
+     */
+    private $validator;
+
+    public function __construct(ProjectRepository $repository, ProjectValidator $validator){
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+
+    public function create(array $data){
+
+        try{
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->create($data);
+        } catch (ValidatorException $e) {
+
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        }
+
+        //enviar email
+        //dispara notificacao
+
+    }
+
+    public function update(array $data, $id){
+
+        try{
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->update($data, $id);
+        } catch (ValidationException $e) {
+
+            return [
+                'error' => true,
+                'message' => $e->geMessageBag()
+            ];
+        }
+
+    }
+}
---
+++ app/Validators/ProjectValidator.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robinho de Morais
+ * Date: 01/09/2015
+ * Time: 21:26
+ */
+
+namespace CodeProject\Validators;
+
+
+use Prettus\Validator\LaravelValidator;
+
+class ProjectValidator extends LaravelValidator {
+    protected $rules = [
+        'owner_id' => 'required|integer',
+        'client_id' => 'required|integer',
+        'name' => 'required',
+        'progress' => 'required',
+        'status' => 'required',
+        'due_date' => 'required'
+    ];
+}
