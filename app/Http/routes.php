8a137aa56398fdd97fcc5d742cd873a6719bc678
robinhodemorais <robinhodemorais@gmail.com>
2015-09-02 23:18:18 -0300

    Atualizando Projects e Models

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,17 @@ Route::post('client', 'ClientController@store');
 Route::get('client/{id}', 'ClientController@show');
 Route::delete('client/{id}', 'ClientController@destroy');
 Route::put('client/{id}', 'ClientController@update');
+
+Route::get('project/{id}/note', 'ProjectNoteController@index');
+Route::post('project/{id}/note', 'ProjectNoteController@store');
+Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
+Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
+Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@delete');
+
+Route::get('project', 'ProjectController@index');
+Route::post('project', 'ProjectController@store');
+Route::get('project/{id}', 'ProjectController@show');
+Route::delete('project/{id}', 'ProjectController@destroy');
+Route::put('project/{id}', 'ProjectController@update');
+
+
--- app/Providers/CodeProjectRepositoryProvider.php
+++ app/Providers/CodeProjectRepositoryProvider.php
@@ -30,5 +30,8 @@ class CodeProjectRepositoryProvider extends ServiceProvider
 
         $this->app->bind(\CodeProject\Repositories\ProjectRepository::class,
             \CodeProject\Repositories\ProjectRepositoryEloquent::class);
+
+        $this->app->bind(\CodeProject\Repositories\ProjectNoteRepository::class,
+            \CodeProject\Repositories\ProjectNoteRepositoryEloquent::class);
     }
 }
