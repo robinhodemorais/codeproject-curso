37ca745525d4218824afb09ba914d286ec63247a
Robinho <robinhodemorais@gmail.com>
2015-10-13 03:29:28 +0000

    4ª Fase do Projeto correçao

---
+++ app/Transformers/ClientTransformer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robinho
+ * Date: 11/10/2015
+ * Time: 21:12
+ */
+
+namespace CodeProject\Transformers;
+
+use CodeProject\Entities\Client;
+use League\Fractal\TransformerAbstract;
+
+class ClientTransformer extends TransformerAbstract
+{
+
+    /*
+     * Transformer, transforma as informações da maneira que vc queira apresentar
+     *
+     */
+
+    public function transform(Client $client){
+        return [
+            'client_id' => $client->id,
+            'name' => $client->name,
+            'responsible' => $client->responsible,
+            'email' => $client->email,
+            'phone' => $client->phone,
+            'address' => $client->address,
+            'obs' => $client->obs,
+        ];
+    }
+
+
+
+}
