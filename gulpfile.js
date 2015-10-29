2e5790fc486c41fd442b189435f442a88229d8c4
Robinho <robinhodemorais@gmail.com>
2015-10-29 00:29:16 +0000

    GulpFile.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,24 +15,24 @@ var config = {
 config.bower_path = config.assets_path + '/../bower_components';
 
 config.build_path_js = config.build_path + '/js';
-config.build_vendor_path_js = config.build_vendor_path_js + '/vendor';
+config.build_vendor_path_js = config.build_path_js + '/vendor';
 config.vendor_path_js = [
-    config.bower_path + '/jquery/dist/jquery.min.js',
-    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
- config.bower_path + '/angular/angular.min.js',
- config.bower_path + '/angular-route/angular-route.min.js',
- config.bower_path + '/angular-resource/angular-resource.min.js',
- config.bower_path + '/angular-animate/angular-animate.min.js',
- config.bower_path + '/angular-messages/angular-messages.min.js',
- config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
- config.bower_path + '/angular-strap/modules/navbar.min.js',
+     config.bower_path + '/jquery/dist/jquery.min.js',
+     config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
+     config.bower_path + '/angular/angular.min.js',
+     config.bower_path + '/angular-route/angular-route.min.js',
+     config.bower_path + '/angular-resource/angular-resource.min.js',
+     config.bower_path + '/angular-animate/angular-animate.min.js',
+     config.bower_path + '/angular-messages/angular-messages.min.js',
+     config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+     config.bower_path + '/angular-strap/modules/navbar.min.js',
 ];
 
 config.build_path_css = config.build_path + '/css';
-config.build_vendor_path_css = config.build_vendor_path_css + '/vendor';
+config.build_vendor_path_css = config.build_path_css + '/vendor';
 config.vendor_path_css = [
- config.bower_path + '/bootstrap/dist/css/bootstrap.min.js',
- config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.js'
+     config.bower_path + '/bootstrap/dist/css/bootstrap.min.js',
+     config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.js'
 ];
 
 gulp.task('copy-styles', function () {
