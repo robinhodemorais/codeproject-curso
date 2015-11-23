52d6400fc121b7b9818d680d1d451ed304eaa984
vagrant <vagrant@homestead>
2015-11-23 01:14:57 +0000

    Crud do ProjectNote

--- resources/assets/js/controllers/project-note/projectNoteList.js
+++ resources/assets/js/controllers/project-note/projectNoteList.js
@@ -1,5 +1,6 @@
 angular.module('app.controllers')
-    .controller('ProjectNoteListController',['$scope','$routeParams' ,'ProjectNote', function($scope, $routeParams, ProjectNote){
+    .controller('ProjectNoteListController',[
+        '$scope','$routeParams' ,'ProjectNote', function($scope, $routeParams, ProjectNote){
         $scope.projectNotes = ProjectNote.query({id: $routeParams.id});
         //console.log($scope.projectNotes);
 
--- resources/assets/js/services/projectNote.js
+++ resources/assets/js/services/projectNote.js
@@ -2,7 +2,7 @@ angular.module('app.services')
     .service('ProjectNote',['$resource','appConfig', function($resource,appConfig){
         return $resource(appConfig.baseUrl + '/project/:id/note/:idNote',{
             id: '@id',
-            idNote: 'idNote'
+            idNote: '@idNote'
         },{
             update: {
                 method: 'PUT'
