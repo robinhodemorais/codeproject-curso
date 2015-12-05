dc43942415e3cd6038bb0935eaaef02f36f76161
robinhodemorais <robinhodemorais@gmail.com>
2015-12-04 22:25:49 -0200

    Aula 6.7-Realizando primeiro cadastro de project

--- resources/assets/js/controllers/project/projectNew.js
+++ resources/assets/js/controllers/project/projectNew.js
@@ -1,13 +1,14 @@
 angular.module('app.controllers')
     .controller('ProjectNewController',
-    ['$scope', '$location', 'Project','Client','appConfig',
-        function($scope, $location, Project, Client, appConfig){
+    ['$scope', '$location','$cookies','Project','Client','appConfig',
+        function($scope, $location, $cookies, Project, Client, appConfig){
             $scope.project = new Project();
             $scope.clients = Client.query();
             $scope.status = appConfig.project.status;
 
             $scope.save = function(){
                 if($scope.form.$valid) {
+                    $scope.project.owner_id = $cookies.getObject('user').id;
                     $scope.project.$save().then().then(function () {
                         $location.path('/projects/');
                     });
