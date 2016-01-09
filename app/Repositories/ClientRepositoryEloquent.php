efb3a28f85e8d239f2ed1c0589866431fe746575
robinhodemorais <robinhodemorais@gmail.com>
2016-01-09 10:44:47 -0200

    Ajustando a pesquisa de cliente

--- app/Repositories/ClientRepositoryEloquent.php
+++ app/Repositories/ClientRepositoryEloquent.php
@@ -10,6 +10,7 @@ namespace CodeProject\Repositories;
 
 use CodeProject\Entities\Client;
 use CodeProject\Presenters\ClientPresenter;
+use Prettus\Repository\Criteria\RequestCriteria;
 use Prettus\Repository\Eloquent\BaseRepository;
 
 class ClientRepositoryEloquent extends BaseRepository implements ClientRepository {
@@ -26,4 +27,11 @@ class ClientRepositoryEloquent extends BaseRepository implements ClientRepositor
         return ClientPresenter::class;
     }
 
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
 }
