52271fd54c2c3f4a61fa9312081fe6d0dbbfad99
robinhodemorais <robinhodemorais@gmail.com>
2016-01-20 23:30:08 -0200

    Trabalhando com eventos

--- resources/assets/js/directives/projectFileDownload.js
+++ resources/assets/js/directives/projectFileDownload.js
@@ -1,11 +1,38 @@
 angular.module('app.directives')
     .directive('projectFileDownload',
-    ['appConfig', 'ProjectFile',  function(appConfig,ProjectFile){
+    ['$timeout','appConfig', 'ProjectFile',  function($timeout,appConfig,ProjectFile){
         return {
             restrict: 'E',
             templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
             link: function(scope, element, attr){
+                var anchor = element.children()[0];
+                /*Evento*/
+                scope.$on('salvar-arquivo',function(event,data){
 
+                     $(anchor).removeClass('disabled');
+                     $(anchor).text('Save File');
+
+                     /*
+                     Indica que temos um dado binário no href e indica que está com o base64
+                     */
+
+                     $(anchor).attr({
+                        href: 'data:application-octet-stream;base64,'+ data.file,
+                        download: data.name
+                     });
+
+                     /*TimeOut para dar um leg de segundo para não apresentar erro*/
+                     $timeout(function(){
+
+                        /* após executar a primeira vez, irá limpar o $scope.downloadFile para
+                            não ficar num loop */
+                        scope.downloadFile = function (){
+                            $(anchor).text('Download');
+                        };
+                        /*irá pegar a ancora e chamar o metodo de click para clicar em download e já baixar o arquivo*/
+                        $(anchor)[0].click();
+                     });
+                });
             },
             /*
             $element = Serviço que permite ter acesso na diretiva
@@ -13,8 +40,8 @@ angular.module('app.directives')
                                                    temos somente 1 elemento então colocamos 0, pois ele é
                                                    o elemento zero
             */
-            controller: ['$scope','$element','$attrs','$timeout',
-                function($scope,$element,$attrs,$timeout){
+            controller: ['$scope','$element','$attrs',
+                function($scope,$element,$attrs){
                     /*
                       desabilita o botão para o download do arquivo
                     */
@@ -24,28 +51,7 @@ angular.module('app.directives')
                         $(anchor).text('Loading...');
 
                         ProjectFile.download({id: null, idFile: $attrs.idFile}, function(data){
-                            $(anchor).removeClass('disabled');
-                            $(anchor).text('Save File');
-
-                            /*
-                              Indica que temos um dado binário no href e indica que está com o base64
-                              */
-
-                            $(anchor).attr({
-                                href: 'data:application-octet-stream;base64,'+ data.file,
-                                download: data.name
-                            });
-                            /*TimeOut para dar um leg de segundo para não apresentar erro*/
-                            $timeout(function(){
-
-                                 /* após executar a primeira vez, irá limpar o $scope.downloadFile para
-                                    não ficar num loop */
-                                $scope.downloadFile = function (){
-
-                                };
-                                /*irá pegar a ancora e chamar o metodo de click para clicar em download e já baixar o arquivo*/
-                                $(anchor)[0].click();
-                            });
+                            $scope.$emit('salvar-arquivo',data);
                         });
                     }
             }]
