659bf5de5525a7b214cd0123a7ba4e83f8306299
robinhodemorais <robinhodemorais@gmail.com>
2016-03-05 00:05:58 -0300

    Configurando realtime no painel

---
+++ app/Events/TaskWasIncluded.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace  CodeProject\Events;
+
+use CodeProject\Entities\ProjectTasks;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class TaskWasIncluded extends Event implements ShouldBroadcast{
+
+    //utilizando esse metodo o laravel pega todas as variaveis publicas e serealiza
+    use SerializesModels;
+
+    public $task;
+
+    public function __construct(ProjectTasks $task){
+        $this->task = $task;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array
+     */
+    /*
+     * Canal que cia com o serviço de real time
+     */
+    public function broadcastOn(){
+        return ['user.'.\Authorizer::getResourceOwnerId()];
+    }
+}
